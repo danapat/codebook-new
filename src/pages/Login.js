277249540e10318dae3c9c48c86c322bcdd028a6
danapat <bogdana.pat@gmail.com>
2025-04-26 01:57:13 +0200

    Guest Login In ENV

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,8 +26,8 @@ export const Login = () => {
 
   async function handleLoginGuest() {
 
-    email.current.value = "someone@gmail.com";
-    password.current.value = "learnreact";
+    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
+    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
     try {
       const authDetail = {
         email: email.current.value,
