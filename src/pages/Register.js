f7d5febd275467b6074e17b78ff5d8a35adabf36
danapat <bogdana.pat@gmail.com>
2025-04-26 02:03:06 +0200

    g

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { useTitle } from "../hooks/useTitle";
 import { register } from "../services";
+import { useTitle } from "../hooks/useTitle";
 
 export const Register = () => {
   useTitle("Register");
